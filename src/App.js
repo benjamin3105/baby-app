9c4c416cebee00208e82f7c797eaa2f27825c3b0
Benjamin [TGP] <benjamin@thegoodplace.nl>
2022-10-07 17:10:55 +0200

    useRef edit to date field

--- src/App.js
+++ src/App.js
@@ -5,11 +5,13 @@ function App() {
 
   const [date, setDate] = useState('')
   const [daysleft, setDaysLeft] = useState('')
-  const chosenDate = useRef()
-
   const today = new Date()
+  const inputElement = useRef()
 
+
+  // format the date to yyyy-mm-dd
   function formatDate(date, format) {
+
     const map = {
         mm: date.getMonth() + 1,
         dd: date.getDate(),
@@ -17,15 +19,24 @@ function App() {
         yyyy: date.getFullYear()
     }
 
-    // add 0 to day < 10
+    // add 0 to day < 10 e.g 6 becomes 06
     if(map.dd < 10) {
         map.dd = '0' + map.dd
     }
 
     return format.replace(/mm|dd|yyyy/gi, matched => map[matched])
+
   }
 
-  function calculateDays() {
+  function test() {
+    console.log(inputElement.current.value)
+    
+  }
+
+  // calculate the days between today and chose date
+  function calculateDays(e) {
+
+    setDate(inputElement.current.value)
     
     const start = new Date(formatDate(today, 'yyyy-mm-dd'))
     const end = new Date(date)
@@ -36,9 +47,12 @@ function App() {
         let TotalDays = Math.ceil(difference / (1000 * 3600 * 24))
         return TotalDays
     }
+
     setDaysLeft(days(start, end))
     
   }
+
+
 
   return (
     <div className="App">
@@ -47,13 +61,19 @@ function App() {
       <div className=''>{daysleft} Days left!</div>
 
       <input 
+      type="text"
       
+      onChange={test}
+      />
+
+      <input 
       type="date" 
       id="start" 
       name="baby-time"
       min={formatDate(today, 'yyyy-mm-dd')}  
       max="2025-12-31"
-      onChange={(e) => setDate(e.target.value)}
+      ref={inputElement}
+      onChange={calculateDays}
       />
 
       <button onClick={calculateDays}>How many days?</button>
